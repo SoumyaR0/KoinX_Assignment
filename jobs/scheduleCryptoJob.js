df757e5a6589dd28fcffd38dd8964cb308571878
SoumyaR0 <soumyaranjanpatra730@gmail.com>
2025-01-11 13:14:17 +0530

    final push

--- jobs/scheduleCryptoJob.js
+++ jobs/scheduleCryptoJob.js
@@ -32,7 +32,7 @@ const fetchCryptoData = async () => {
 
 const scheduleCryptoJob = () => {
   // Run the job every 2 hours
-  cron.schedule(" */200 * * * * *", async () => {
+  cron.schedule("0 */2 * * *", async () => {
     console.log("Fetching cryptocurrency data...");
 
     const cryptoData = await fetchCryptoData();
